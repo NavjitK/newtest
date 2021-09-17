521f1e038fd8cbc04eaee2b3a399cd03157b6a6b
Navjit Kaur <navjitkaur297@gmail.com>
2021-09-17 16:58:05 +0530

    Update remote.java
    updated

--- selenium/src/main/java/test/remote.java
+++ selenium/src/main/java/test/remote.java
@@ -49,15 +49,15 @@ public class remote {
 		  System.out.println("Clicked on Add new Employee");
 		  Thread.sleep(4000);
 		  driver.findElement(By.xpath("//input[@name='name']")).click();
-		  driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Harpreet");
+		  driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Yogesh");
 		  System.out.println("Added name");
 		  Thread.sleep(4000);
 		  driver.findElement(By.xpath("//textarea[@name='address']")).click();
-		  driver.findElement(By.xpath("//textarea[@name='address']")).sendKeys("Punjab");
+		  driver.findElement(By.xpath("//textarea[@name='address']")).sendKeys("Haryana");
 		  System.out.println("Added Address");
 		  Thread.sleep(4000);
 		  driver.findElement(By.xpath("//input[@name='salary']")).click();
-		  driver.findElement(By.xpath("//input[@name='salary']")).sendKeys("10000");
+		  driver.findElement(By.xpath("//input[@name='salary']")).sendKeys("12000");
 		  System.out.println("Entered Salary");
 		  Thread.sleep(4000);
 		  driver.findElement(By.xpath("//input[@type='submit']")).click();
